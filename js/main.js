4a2e343049c87732238e1e9feeb8825c16b82f6d
TrvMarco <trevisanello.marco@gmail.com>
2022-06-24 15:49:42 +0200

    prova axios

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 const app = new Vue({
     el: "#app",
     data:{
-        
+        albumArray: []
     },
     created(){
         axios.get('http://localhost/php-ajax-dischi/server/api.php')
         .then(function (response) {
-            console.log(response.data);
+            response.data.forEach(elm => {
+                console.log(elm)
+                const singleAlbum = elm; 
+                console.log(singleAlbum)
+                this.albumArray.push(this.singleAlbum)
+            });
+            // console.log(response.data);
         })
         .catch(function (error) {
             console.log(error);
